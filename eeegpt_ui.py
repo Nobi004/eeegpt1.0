aad2d9094f8928e0df4602be9f8001c1d77241f5
Mahmudun Nobi <mdmnb435@gmail.com>
2025-05-16 21:13:17 +0600

    change owner header to subheader

--- eeegpt_ui.py
+++ eeegpt_ui.py
@@ -73,7 +73,7 @@ class GroqLLM(LLM):
 
 def main():
     st.title("Welcme to EEEGPT")
-    st.header("Made by Md. Mahmudun Nobi")
+    st.subheader("Made by Md. Mahmudun Nobi")
     st.subheader("Your AI Assistant for Electrical and Electronic Engineering.")
     st.write("Ask me anything related to your studies, and I'll do my best to help you!")
     if 'messages' not in st.session_state:
