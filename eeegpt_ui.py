f910b46ed09a84be4a929f03ea0b99f9fccadce9
Mahmudun Nobi <mdmnb435@gmail.com>
2025-05-16 22:05:30 +0600

    Update eeegpt_ui.py

--- eeegpt_ui.py
+++ eeegpt_ui.py
@@ -74,7 +74,7 @@ class GroqLLM(LLM):
 def main():
     st.title("Welcme to EEEGPT v1.0")
     st.subheader("Made by Md. Mahmudun Nobi")
-    st.subheader("Your AI Assistant for Electrical and Electronic Engineering.")
+    st.subheader("Your AI Assistant for Electrical and Electronic Engineering study.")
     # st.write("Ask me anything related to your studies, and I'll do my best to help you!")
     if 'messages' not in st.session_state:
         st.session_state.messages = []
